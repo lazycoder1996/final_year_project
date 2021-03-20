197df43d011654b453e9c5f73faf91abb0988649
lazycoder1996 <mujappiah@gmail.com>
2021-03-20 17:14:36 +0000

    Create curveRanging.dart

---
+++ lib/tools/curveRanging.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CurveRanging extends StatefulWidget {
+  @override
+  _CurveRangingState createState() => _CurveRangingState();
+}
+
+class _CurveRangingState extends State<CurveRanging> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
