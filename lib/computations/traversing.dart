603688392c7b5e6732ab2962ca1fb5dd12cc4191
lazycoder1996 <mujappiah@gmail.com>
2021-04-05 20:41:46 +0000

    Update traversing.dart

--- lib/computations/traversing.dart
+++ lib/computations/traversing.dart
@@ -2,7 +2,134 @@ import 'dart:math';
 import 'package:final_project/functions.dart';
 import 'package:flutter/material.dart';
 
-void loopTraverseComputation() {}
+num converter = (pi / 180);
+
+List computeIncludedAngles({List<dynamic> circleReadings}) {
+  List<dynamic> doubleIncludedAngles = [];
+  List<dynamic> includedAngles = [];
+  circleReadings.insert(0, 0);
+  int n = 2;
+  num angle;
+  int size = circleReadings.length;
+  // double included angles
+  try {
+    while (n < size) {
+      if (n % 2 == 0 && n % 4 != 0) {
+        angle = (circleReadings[n] - circleReadings[n - 1]) % 360;
+      } else if (n % 2 == 0 && n % 4 == 0) {
+        angle = (circleReadings[n - 1] - circleReadings[n]) % 360;
+      }
+      doubleIncludedAngles.add(angle);
+      n += 2;
+    }
+  } catch (e) {}
+  n = 0;
+  num meanAngle;
+  try {
+    while (n < doubleIncludedAngles.length) {
+      meanAngle = doubleIncludedAngles[n] + doubleIncludedAngles[n + 1];
+      includedAngles.add(meanAngle / 2);
+      n += 2;
+    }
+  } catch (e) {}
+  return includedAngles;
+}
+
+List<dynamic> computeBearings(
+    {num initialBearing, List<dynamic> includedAngles}) {
+  List<dynamic> results = [initialBearing];
+  int size = includedAngles.length;
+  int n = 0;
+  try {
+    while (n <= size) {
+      results.add((backBearing(results[n]) + includedAngles[n]) % 360);
+      n++;
+    }
+  } catch (e) {}
+  return results;
+}
+
+List<dynamic> adjustBearings({List<dynamic> initialBearings}) {
+  num error = initialBearings.last - initialBearings.first;
+  num adjustment = error / (initialBearings.length - 1);
+  List<dynamic> adjPerStation = [];
+  List<dynamic> finalBearing = [];
+  for (var i in initialBearings) {
+    adjPerStation.add(-adjustment * initialBearings.indexOf(i));
+  }
+  for (int i = 0; i < adjPerStation.length; i++) {
+    finalBearing.add((initialBearings[i] + adjPerStation[i]) % 360);
+  }
+  return [adjPerStation, finalBearing];
+}
+
+List<List<dynamic>> computeLatDdep(
+    {List<dynamic> bearings, List<dynamic> distances}) {
+  List<List<dynamic>> latDep = [[], []];
+  try {
+    for (int i = 0; i < bearings.length; i++) {
+      latDep[0].add(distances[i] * cos(converter * bearings[i + 1]));
+      latDep[1].add(distances[i] * sin(converter * bearings[i + 1]));
+    }
+  } catch (e) {}
+  return latDep;
+}
+
+List<List<dynamic>> adjustLatDep(
+    {String adjustmentMethod,
+    List<List<dynamic>> initialLatDep,
+    List<dynamic> distances}) {
+  List<dynamic> adjLat = [];
+  List<dynamic> adjDep = [];
+  List<dynamic> correctedLat = [];
+  List<dynamic> correctedDep = [];
+
+  if (adjustmentMethod == 'Bowditch') {
+    num sumLat = 0;
+    num sumDistances = 0;
+    num sumDep = 0;
+    for (var i in initialLatDep[0]) {
+      sumLat += i;
+    }
+    for (var i in initialLatDep[1]) {
+      sumDep += i;
+    }
+    for (var i in distances.sublist(0, distances.length - 1)) {
+      sumDistances += i;
+    }
+    int n = 0;
+    int size = initialLatDep[0].length;
+    try {
+      while (n < size - 1) {
+        adjLat.add((distances[n] / sumDistances) * -sumLat);
+        correctedLat.add(adjLat[n] + initialLatDep[0][n]);
+        adjDep.add((distances[n] / sumDistances) * -sumDep);
+        correctedDep.add(adjDep[n] + initialLatDep[1][n]);
+        n++;
+      }
+      adjLat.add(0);
+      adjDep.add(0);
+      correctedLat.add(initialLatDep[0].last);
+      correctedDep.add(initialLatDep[1].last);
+    } catch (e) {}
+  } else {}
+  return [adjLat, correctedLat, adjDep, correctedDep];
+}
+
+List<List<dynamic>> computeNorthingsEastings(
+    {List<List<dynamic>> latDep, List<dynamic> controls}) {
+  List<List<dynamic>> results = [
+    [controls[0]],
+    [controls[1]]
+  ];
+  try {
+    for (int i = 0; i <= latDep[1].length; i++) {
+      results[0].add(results[0][i] + latDep[1][i]);
+      results[1].add(results[1][i] + latDep[3][i]);
+    }
+  } catch (e) {}
+  return results;
+}
 
 void linkTraverseComputation(
     {List<List<dynamic>> data,
@@ -89,7 +216,6 @@ void linkTraverseComputation(
   } catch (e) {}
 
   // latitudes and departures
-  num converter = (pi / 180);
 
   List<dynamic> latitudes = [];
   List<dynamic> departures = [];
