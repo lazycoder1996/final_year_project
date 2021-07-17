c6ef9ebfc2770c3c5a8f327e4d5ed3bdd5f75b70
lazycoder1996 <70382598+lazycoder1996@users.noreply.github.com>
2021-07-17 22:02:00 +0000

    Update loopTraverseComp.dart

--- lib/computations/traversing/loopTraverseComp.dart
+++ lib/computations/traversing/loopTraverseComp.dart
@@ -1,164 +1,157 @@
-// import 'package:final_project/computations/traversing/travFunctions.dart';
-// import 'package:final_project/genFunctions.dart';
+import 'travFunctions.dart';
 
-// void linkTraverse({String adjustBy}) {
-//   var traverseData = readFile('C:/Users/muj/Desktop/MsFiles/link_traverse.csv');
-//   var dataSize = traverseData.length;
-//   var backsightData = [];
-//   var foresightData = [];
-//   var stationData = [];
-//   var circleReadings = [];
-//   var distanceData = [];
-//   var controls = [];
-//   for (var i = 1; i < dataSize; i++) {
-//     backsightData.add(traverseData[i][0]);
-//     foresightData.add(traverseData[i][2]);
-//     stationData.add(traverseData[i][1]);
-//     circleReadings.add(traverseData[i][4]);
-//     distanceData.add(traverseData[i][5]);
-//     controls.add(traverseData[i][6]);
-//   }
-//   distanceData.removeWhere((element) => element == '');
-//   controls.removeWhere((element) => element == '');
-//   stationData.removeWhere((element) => element == '');
-//   foresightData.removeWhere((element) => element == '');
-//   print('station data is $stationData');
-//   // initial distance and bearing
-//   var firstControl = controls[0].toString().split(',');
-//   var secondControl = controls[1].toString().split(',');
-
-//   var initDistBear = forwardGeodetic(
-//       num.parse(firstControl[0]),
-//       num.parse(firstControl[1]),
-//       num.parse(secondControl[0]),
-//       num.parse(secondControl[1]));
-//   print('initial bearing and distance is $initDistBear');
-
-//   // ending distance and bearing
-//   var endFirstControl = controls[2].split(',');
-//   var endSecondControl = controls.last.split(',');
-
-//   var finalDistBear = forwardGeodetic(
-//       num.parse(endFirstControl[0]),
-//       num.parse(endFirstControl[1]),
-//       num.parse(endSecondControl[0]),
-//       num.parse(endSecondControl[1]));
-//   print('final distance and bear is $finalDistBear');
-
-//   print('circle readings are $circleReadings');
-//   // calculating included angles
-//   var includedAngles = computeIncludedAngles(
-//     circleReadings: circleReadings,
-//   );
-//   print('included angles are $includedAngles');
-
-//   // adjust included angles
-//   var adjustedIncludedAngles = adjustIncludedAnglesLink(
-//       includedAngles: includedAngles,
-//       finalForwardBearing: finalDistBear[1],
-//       initialBackBearing: backBearing(initDistBear[1]));
-//   print('adjusted included angles are $adjustedIncludedAngles');
-//   // calculating initial bearings
-//   var initialBearings = computeBearings(
-//       includedAngles: adjustedIncludedAngles[1],
-//       initialBearing: initDistBear[1]);
-//   print('initial bearings are $initialBearings');
-//   // adjusting initial bearings
-//   // var adjustedBearings = adjustBearings(
-//   //     initialBearings: initialBearings,
-//   //     typeOfTraverse: 'Link',
-//   //     endBearing: finalDistBear[1]);
-
-//   // departure and latitudes
-//   var initDepLat = computeDepLat(
-//     bearings: initialBearings.sublist(1, initialBearings.length),
-//     distances: distanceData,
-//   );
-//   // print('adjusted bear are $adjustedBearings');
-//   print('distances are $distanceData');
-//   print('dep lat is $initDepLat');
-
-//   // adjust dep and lat
-//   var adjustedDepLat = adjustDepLat(
-//       adjustmentMethod: adjustBy,
-//       checkControls: [
-//         secondControl[0],
-//         secondControl[1],
-//         endFirstControl[0],
-//         endFirstControl[1]
-//       ],
-//       distances: distanceData,
-//       initialDepLat: initDepLat,
-//       typeOfTraverse: 'Link');
-//   print('adjusted dep lat are $adjustedDepLat');
-
-//   // final coordinates
-//   var finalCoordinates = computeEastingNorthing(
-//     typeOfTraverse: 'Link',
-//     controls: [num.parse(secondControl[0]), num.parse(secondControl[1])],
-//     depLat: adjustedDepLat,
-//   );
-//   print('final coordinates are $finalCoordinates');
-
-//   List<List<dynamic>> output = [];
-//   output.insert(0, [
-//     'From',
-//     'To',
-//     'Included Angle',
-//     'Corr to Inc. Angle',
-//     'Adjusted Inc. Angle',
-//     'Distance',
-//     'Bearing',
-//     'Departure',
-//     'Corr to Dep',
-//     'Adjusted Dep',
-//     'Latitude',
-//     'Corr to Lat',
-//     'Adjusted Lat',
-//     'Easting',
-//     'Northing'
-//   ]);
-//   output.insert(1, [
-//     '',
-//     stationData.first,
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     '',
-//     secondControl.first,
-//     secondControl[1]
-//   ]);
-
-//   var n = 0;
-//   var m = 1;
-//   try {
-//     while (n <= finalCoordinates[0].length) {
-//       output.insert(m + 1, [
-//         stationData[n],
-//         stationData[n + 1],
-//         deg2DMS(includedAngles[n]),
-//         deg2DMS(adjustedIncludedAngles[0][n]),
-//         deg2DMS(adjustedIncludedAngles[1][n]),
-//         distanceData[n],
-//         deg2DMS(initialBearings[n]),
-//         (initDepLat[0][n]).toStringAsFixed(4),
-//         (adjustedDepLat[0][n]).toStringAsFixed(4),
-//         (adjustedDepLat[1][n]).toStringAsFixed(4),
-//         (initDepLat[1][n]).toStringAsFixed(4),
-//         (adjustedDepLat[2][n]).toStringAsFixed(4),
-//         (adjustedDepLat[3][n]).toStringAsFixed(4),
-//         (finalCoordinates[0].sublist(1)[n]).toStringAsFixed(4),
-//         (finalCoordinates[1].sublist(1)[n]).toStringAsFixed(4)
-//       ]);
-//       n++;
-//       m++;
-//     }
-//   } catch (e) {}
-// }
+loopTraverse(
+    {String adjustBy,
+    List<List<dynamic>> traverseData,
+    Map<String, dynamic> rawValues}) {
+  var dataSize = traverseData.length;
+  var backsightData = rawValues['backsight'];
+  var foresightData = rawValues['foresight'];
+  var stationData = rawValues['station'];
+  var circleReadings = rawValues['circle'];
+  var distanceData = rawValues['distance'];
+  var controls = rawValues['control'];
+  // print('backsight data $backsightData');
+  // print('foresight data $foresightData');
+  stationData.removeWhere((element) => element == '');
+  // print('station data is $stationData');
+  stationData.insert(0, stationData.last);
+  stationData.add(stationData[1]);
+  // initialBearing
+  controls.removeWhere((element) => element == '');
+  var backSightControls = controls[0].toString().split(',');
+  var stationControls = controls[1].toString().split(',');
+  var controlData = forwardGeodetic(
+      double.parse(backSightControls[0]),
+      double.parse(backSightControls[1]),
+      double.parse(stationControls[0]),
+      double.parse(stationControls[1]));
+  // adding distance of controls to distanceData
+  distanceData.insert(0, controlData[0]);
+  distanceData.add(controlData[0]);
+  distanceData.removeWhere((element) => element == '');
+  List departure = [
+    '- ${backsightData[0]} (${backSightControls[0]}, ${backSightControls[1]})',
+    '- ${stationData[1]} (${stationControls[0]}, ${stationControls[1]})'
+  ];
+  List closure = [
+    '- ${backsightData[0]} (${backSightControls[0]}, ${backSightControls[1]})',
+    '- ${stationData[1]} (${stationControls[0]}, ${stationControls[1]})'
+  ];
+  // print('distance data are $distanceData');
+  // computing included angles
+  var includedAngles = computeIncludedAngles(circleReadings: circleReadings);
+  num sum = 0;
+  includedAngles.forEach((element) {
+    sum += element;
+  });
+  // print(includedAngles);
+  // print(sum);
+  // adjusted included angles
+  // var adjustedIncludedAngles = adjustIncludedAngles(
+  //     includedAngles: includedAngles, direction: direction);
+  // print('adjusted included angles are $adjustedIncludedAngles');
+  // compute bearings
+  var unadjustedBearings = computeBearings(
+      typeOfTraverse: 'Closed Loop',
+      includedAngles: includedAngles,
+      initialBearing: controlData[1]);
+  // print('initial bearing is ${controlData[1]}');
+  // print('initial bearings are $unadjustedBearings');
+  // adjust bearings
+  print('un be is $unadjustedBearings');
+  var adjustedBearings = adjustBearings(
+      initialBearings: unadjustedBearings['results'],
+      typeOfTraverse: 'Closed Loop');
+  print('adjusted bearings are $adjustedBearings');
+  // compute dep and lat
+  var depLat = computeDepLat(
+      bearings: adjustedBearings['results'][1], distances: distanceData);
+  // print('dep and lat are $depLat');
+  // adjust dep lat
+  var adjustedDepLat = adjustDepLat(
+      typeOfTraverse: 'Closed Loop',
+      adjustmentMethod: adjustBy,
+      distances: distanceData,
+      initialDepLat: depLat,
+      expLoopTransit: [
+        double.parse(backSightControls[0]) - double.parse(stationControls[0]),
+        double.parse(backSightControls[1]) - double.parse(stationControls[1])
+      ]);
+  print('adjusted dep lat are $adjustedDepLat');
+  // computing eastings and northings
+  var finalCoordinates = computeEastingNorthing(
+      controls: [num.parse(stationControls[0]), num.parse(stationControls[1])],
+      depLat: adjustedDepLat['results']);
+  print('final coordinates are $finalCoordinates');
+  finalCoordinates.forEach((element) {
+    print(element.toString());
+  });
+  List<List<dynamic>> output = [];
+  output.insert(0, [
+    'From',
+    'To',
+    'Included Angle',
+    'Distance',
+    'Bearing',
+    'Corr to Bear',
+    'Adjusted Bear',
+    'Departure',
+    'Corr to Dep',
+    'Adjusted Dep',
+    'Latitude',
+    'Corr to Lat',
+    'Adjusted Lat',
+    'Easting',
+    'Northing'
+  ]);
+  includedAngles.insert(0, '');
+  var n = 0;
+  try {
+    while (n <= finalCoordinates[0].length) {
+      // print('start');
+      // print('n: $n');
+      output.add([
+        stationData[n],
+        stationData[n + 1],
+        includedAngles[n],
+        distanceData[n],
+        unadjustedBearings['results'][n],
+        adjustedBearings['results'][0][n],
+        adjustedBearings['results'][1][n],
+        depLat[0][n],
+        adjustedDepLat['results'][0][n],
+        adjustedDepLat['results'][1][n],
+        depLat[1][n],
+        adjustedDepLat['results'][2][n],
+        adjustedDepLat['results'][3][n],
+        finalCoordinates[0][n],
+        finalCoordinates[1][n]
+      ]);
+      n++;
+    }
+  } catch (e) {}
+  // print('end');
+  return [
+    output,
+    {
+      'departure': departure.join("\r\n"),
+      'closure': closure.join('\r\n'),
+      'setup': includedAngles.length,
+      'adj By': adjustBy,
+      'observed final bearing':
+          unadjustedBearings['error'][0].toStringAsFixed(6),
+      'expected final bearing': controlData[1].toStringAsFixed(6),
+      'error': adjustedBearings['error'][0].toStringAsFixed(6),
+      'sum of dep': adjustedDepLat['error'][0].toStringAsFixed(6),
+      'exp sum of dep': adjustedDepLat['error'][2].toStringAsFixed(6),
+      'error in dep': adjustedDepLat['error'][4].toStringAsFixed(6),
+      'sum of lat': adjustedDepLat['error'][1].toStringAsFixed(6),
+      'exp sum of lat': adjustedDepLat['error'][3].toStringAsFixed(6),
+      'error in lat': adjustedDepLat['error'][5].toStringAsFixed(6),
+      'sum dist': adjustedDepLat['error'][6].toStringAsFixed(6),
+      'linear misclose': adjustedDepLat['error'][7].toStringAsFixed(6),
+      'fractional misclose': adjustedDepLat['error'][8],
+    }
+  ];
+}
